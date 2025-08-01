26579c41932e63c8d19930cec7f39e5316fd27de
Caio Silva <caiosiq@submit01.mit.edu>
2025-08-01 14:45:18 -0400

    Cleanup of Files

--- ibm_global_sampler_scripts/backend_analysis.py
+++
@@ -1,9 +0,0 @@
-from qiskit_ibm_runtime import QiskitRuntimeService
-
-service = QiskitRuntimeService()
-backends = service.backends(simulator=False, operational=True)
-for b in backends:
-    props = b.properties()
-    # Example: get average two-qubit gate error
-    errors = [inst for inst in props.qubit_property(1).get("readout_error", [])]
-    print(b.name, "qubits:", b.num_qubits, "2Q errors sample:", errors[:3])
